f8cc604ea7f7d33664b0eee18c68095f6c6cf846
PC <f.o.g@list.ru>
2023-04-11 11:42:51 +0400

    hw done

--- src/main/java/pro/homework23/sky/calculator/CalculatorController.java
+++ src/main/java/pro/homework23/sky/calculator/CalculatorController.java
@@ -1,33 +1,42 @@
 package pro.homework23.sky.calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
-    private int num1;
-    private int num2;
+private final CalculatorServiceImpl calculatorService;
 
-    public CalculatorController(int num1, int num2) {
-        this.num1 = num1;
-        this.num2 = num2;
+    public CalculatorController(CalculatorServiceImpl calculatorService) {
+        this.calculatorService = calculatorService;
     }
 
-    public int plus() {
-        return num1 + num2;
+    @GetMapping("/plus")
+    public String plusService(@RequestParam int num1, @RequestParam int num2) {
+        return String.format("%s +%s = %s", num1, num2, calculatorService.plus(num1,num2));
     }
 
-    public int minus() {
-        return num1 - num2;
-    }
-
-    public int multiply() {
-        return num1 * num2;
-    }
-
-    public String divide() {
-        if (num2 == 0) {
-            return "Деление на 0!";
-        }
-       double s=((double) num1) /((double) num2);
-        return String.format("%s", s);
+    @GetMapping("/minus")
+    public String minusService(@RequestParam int num1, @RequestParam int num2) {
+        return String.format("%s - %s = %s", num1, num2, calculatorService.minus(num1,num2));
     }
 
 
+    @GetMapping
+    public String welcome() {
+        return "<b>Добро пожаловать в калькулятор!</b>";
+    }
+
+    @GetMapping("/multiply")
+    public String multiplyService(@RequestParam int num1, @RequestParam int num2) {
+        return String.format("%s * %s = %s", num1, num2, calculatorService.multiply(num1,num2));
+    }
+
+    @GetMapping("/divide")
+    public String divideService(@RequestParam int num1, @RequestParam int num2) {
+        return String.format("%s / %s = %s", num1, num2, calculatorService.divide(num1,num2));
+    }
 }
--- src/main/java/pro/homework23/sky/calculator/CalculatorService.java
+++ src/main/java/pro/homework23/sky/calculator/CalculatorService.java
@@ -1,42 +1,14 @@
 package pro.homework23.sky.calculator;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+public interface CalculatorService {
 
-@RestController
-public class CalculatorService {
-    @GetMapping
-    public void hello() {
-        System.out.println("<b>Hello</b>");
-    }
+     int plus(int num1,int num2);
 
-    @GetMapping(path = "calculator")
-    public String welcome() {
-        return "<b>Добро пожаловать в калькулятор!</b>";
-    }
+     int minus(int num1,int num2);
 
-    @GetMapping(path = "calculator/plus")
-    public String plusService(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        CalculatorController calculatorController = new CalculatorController(num1, num2);
-        return String.format("%s +%s = %s", num1, num2, calculatorController.plus());
-    }
+     int multiply(int num1,int num2);
 
-    @GetMapping(path = "calculator/minus")
-    public String minusService(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        CalculatorController calculatorController = new CalculatorController(num1, num2);
-        return String.format("%s - %s = %s", num1, num2, calculatorController.minus());
-    }
+     String divide(int num1,int num2);
 
-    @GetMapping(path = "calculator/multiply")
-    public String multiplyService(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        CalculatorController calculatorController = new CalculatorController(num1, num2);
-        return String.format("%s * %s = %s", num1, num2, calculatorController.multiply());
-    }
 
-    @GetMapping(path = "calculator/divide")
-    public String divideService(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        CalculatorController calculatorController = new CalculatorController(num1, num2);
-        return String.format("%s / %s = %s", num1, num2, calculatorController.divide());
-    }
 }
